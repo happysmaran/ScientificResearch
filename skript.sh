7a06310ab997f101819e7aedbf164eb5cae01da9
Smaran <64059540+happysmaran@users.noreply.github.com>
2023-11-28 11:10:15 +0530

    Add files via upload

---
+++ skript.sh
@@ -0,0 +1,39 @@
+echo "java insertion sort"
+javac -d . JavaInsertionSort.java
+java JavaInsertionSort
+sleep 1
+java JavaInsertionSort
+sleep 1
+java JavaInsertionSort
+
+echo "java merge sort"
+javac -d . JavaMergeSort.java
+java JavaMergeSort
+sleep 1
+java JavaMergeSort
+sleep 1
+java JavaMergeSort
+
+echo "java quick sort"
+javac -d . JavaQuickSort.java
+java JavaQuickSort
+sleep 1
+java JavaQuickSort
+sleep 1
+java JavaQuickSort
+
+echo "java selection sort"
+javac -d . JavaSelectionSort.java
+java JavaSelectionSort
+sleep 1
+java JavaSelectionSort
+sleep 1
+java JavaSelectionSort
+
+echo "java linear regression"
+javac -d . JavaRegression.java
+java JavaRegression
+sleep 1
+java JavaRegression
+sleep 1
+java JavaRegression
