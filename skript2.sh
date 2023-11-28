0c6f791acdd27e0c88bfaf0e95a36ac6e03bf25d
Smaran <64059540+happysmaran@users.noreply.github.com>
2023-11-28 11:12:55 +0530

    Add files via upload

---
+++ skript2.sh
@@ -0,0 +1,44 @@
+echo "CPP insertion sort"
+g++ CPPInsertionSort.cpp
+chmod +x a.out
+./a.out
+sleep 1
+./a.out
+sleep 1
+./a.out
+
+echo "CPP merge sort"
+g++ CPPMergeSort.cpp
+chmod +x a.out
+./a.out
+sleep 1
+./a.out
+sleep 1
+./a.out
+
+echo "CPP quick sort"
+g++ CPPQuickSort.cpp
+chmod +x a.out
+./a.out
+sleep 1
+./a.out
+sleep 1
+./a.out
+
+echo "CPP selection sort"
+g++ CPPSelectionSort.cpp
+chmod +x a.out
+./a.out
+sleep 1
+./a.out
+sleep 1
+./a.out
+
+echo "CPP linear regression"
+g++ CPPRegression.cpp
+chmod +x a.out
+./a.out
+sleep 1
+./a.out
+sleep 1
+./a.out
